3e5af14a098b6336dd2dca791e9199ae72f49c0d
Ufuck1903 <ufuk.ocak.1903@gmail.com>
2021-12-05 20:47:53 +0300

    Guild admins want set prefix maybe (sorry for last commit duplicate)

--- src/commands/moderation/SetPrefixCommand.js
+++ src/commands/moderation/SetPrefixCommand.js
@@ -1,11 +1,25 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
-
+const guildModel = require('../../models/guildModel')
 module.exports = class SetPrefixCommand extends BaseCommand {
     constructor() {
         super('prefix', 'moderation', []);
     }
 
-    run(client, message, args) {
-        message.channel.send('setPrefix command works');
+    async run(client, message, args) {
+        if (!args[0]) {
+            return message.reply("Please provide an arg.")
+        }
+        if (!message.member.permissions.has("MANAGE_GUILD")) { //check user permission
+            return message.channel.send("You don't have the permissions to use this command! (MANAGE_GUILD)");
+
+        }
+        if (args[0].length > 5) return message.channel.send("Max 5 characters!");//check arg max 5 characters
+        const check = /^[!-\/:-@\[-`{-~a-zA-Z0-9]{1,4}$/.test(args[0]); //only allowed !-/\:-@ azAZ09
+        if (check === false) return message.channel.send("Can only contain symbol numbers and letters.");
+        const filter = {_id: message.guild.id};
+        const update = {prefix: args[0]};
+        let doc = await guildModel.findOneAndUpdate(filter, update, {useFindAndModify: false});// update user arg on database
+        message.channel.send(`Now prefix is "${args[0]}"`);
+
     }
 }
